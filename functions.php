39f14a2fdf8b732e9e67d56d0fefdaadc8f0a3a2
Philippe Sendze <sendze.philippe@gmail.com>
2023-06-13 18:45:14 +0100

    hooks

--- functions.php
+++ functions.php
@@ -80,3 +80,77 @@ function banniere_titre_func($atts)
 
     return $output;
 }
+
+/* HOOKS FILTERS */
+
+function the_category_filter($categories)
+{
+    return str_replace("Outils", "Tous les outils", $categories);
+}
+
+add_filter('the_category', 'the_category_filter');
+
+
+function the_content_filter($content)
+{
+   if (is_single() && in_category('outils')) {
+      return '<hr><h5>Description</h5>' . $content;
+   }
+   return $content;
+}
+
+add_filter('the_content', 'the_content_filter');
+
+
+function the_excerpt_filter($content)
+{
+   if (is_archive()) {
+               return $content."<div class='more-excerpt'><a href='".get_the_permalink()."'>En savoir plus sur l'outil</a></div>";
+   }
+   return $content;
+}
+
+add_filter('the_excerpt', 'the_excerpt_filter');
+
+
+function paginate_links_filter($r)
+{
+    return "Pages : ".$r;
+}
+
+add_filter('paginate_links_output', 'paginate_links_filter');
+
+
+
+/* HOOKS ACTIONS */
+
+$shown = false;
+function bricotips_intro_section_action()
+{
+    global $shown;
+    if (is_archive() && !$shown):
+        ?>
+        <p class="intro">Vous trouverez dans cette page la liste de tous les outils que nous avons référencée pour le
+            moment. La liste n'est pas exhaustive, mais s'enrichira au fur et à mesure.</p>
+    <?php
+        $shown = true;
+    endif;
+}
+
+add_action('bricotips_intro_section', 'bricotips_intro_section_action');
+
+
+function loop_end_action()
+{
+    if (is_archive()):
+        ?>
+        <p class="after-loop">
+            <?php
+                echo do_shortcode('[banniere-titre src="http://localhost/bricotips-2/wp-content/uploads/2023/06/banniere-image.webp" titre="BricoTips"]');
+            ?>
+        </p>
+        <?php
+   endif;
+}
+
+add_action('loop_end', 'loop_end_action');
